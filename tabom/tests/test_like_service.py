5e508ccc82d20843dad66bb58016fcecc90804e8
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-08 21:58:27 +0900

    Fixed Inplement do like

--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -7,15 +7,14 @@ from tabom.services.like_service import do_like
 class TestLikeService(TestCase):
 
     def test_a_user_can_like_an_article(self) -> None:
-        pass
         # Given
-        # user = User.objects.create(name="test")
-        # article = Article.objects.create(title="test article")
-        #
-        # # When
-        # like = do_like(user.id, article.id)
-        #
-        # # Then
-        # self.assertIsNotNone(like.id)
-        # self.assertEqual(user.id, like.user_id)
-        # self.assertEqual(article.id, like.article_id)
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test article")
+
+        # When
+        like = do_like(user.id, article.id)
+
+        # Then
+        self.assertIsNotNone(like.id)
+        self.assertEqual(user.id, like.user_id)
+        self.assertEqual(article.id, like.article_id)
