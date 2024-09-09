919330eff3d1add4960abf3b6f36555411d46bee
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-09 11:27:32 +0900

    Add unique constraint

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -2,6 +2,5 @@
 from tabom.models import Like
 
 
-def do_like(user_id: int, like_id: int) -> Like:
-    like = Like.objects.create(user_id=user_id, article_id=like_id)
-    return like
+def do_like(user_id: int, article_id: int) -> Like:
+    return Like.objects.create(user_id=user_id, article_id=article_id)
--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 from tabom.models import Article, User
@@ -18,3 +19,15 @@ class TestLikeService(TestCase):
         self.assertIsNotNone(like.id)
         self.assertEqual(user.id, like.user_id)
         self.assertEqual(article.id, like.article_id)
+
+    def test_a_user_can_like_an_article_only_once(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test article")
+
+        # Ex
+        like = do_like(user.id, article.id)
+        with self.assertRaises(IntegrityError):
+            like2 = do_like(user.id, article.id)
+
+        # Then
