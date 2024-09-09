a2bb5af167f7eb3849e514fe81ded8d1e4e94cab
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-09 13:49:53 +0900

    undo like

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -4,3 +4,7 @@ from tabom.models import Like
 
 def do_like(user_id: int, article_id: int) -> Like:
     return Like.objects.create(user_id=user_id, article_id=article_id)
+
+
+def undo_like(user_id: int, article_id: int) -> tuple[int, dict[str, int]]:
+    return Like.objects.filter(user_id=user_id, article_id=article_id).delete()
