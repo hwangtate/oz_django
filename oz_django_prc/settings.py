578ceef6c666492161c563d3600072935cdbde2d
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-11 11:58:47 +0900

    Implement prefetch

--- oz_django_prc/settings.py
+++ oz_django_prc/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "oz_django",
         "USER": "root",
-        "PASSWORD": "1025",
+        "PASSWORD": "1234",
         "HOST": "localhost",
         "PORT": "3306",
     }
